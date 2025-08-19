3803bcc861520dfa8d6799aa10b92c94b1a2cdac
Zardozodraz <nicolas.vansteene@gmail.com>
2025-08-19 12:10:15 +0200

    Dynemic resising fixed

--- HUD_SessionStats.py
+++ HUD_SessionStats.py
@@ -61,7 +61,7 @@ class SessionHUD:
         
         self.root = tk.Tk()
         self.root.title("HUD Session")
-        self.root.geometry(f"{round(largeur * 0.1)}x{round(hauteur * 0.14)}+1710+10") # "200x150+1710+10"
+        self.root.geometry(f"{round(largeur * 0.1)}x{round(hauteur * 0.14)}+{round(largeur * 0.9)}+10") # "200x150+1710+10"
         self.root.configure(bg="black")
         self.root.wm_attributes("-topmost", True)
         self.root.attributes("-alpha", 0.85)
